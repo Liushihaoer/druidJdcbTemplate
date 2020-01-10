e3ce7a6a4e2bbdb339657b2207d6f2c9aa9d15ad
刘世豪 <liushihao@dfsoft.com.cn>
2020-01-10 16:32:40 +0800

    更新oracle连接配置

--- src/main/java/com/dcsoft/aims/utils/DataSourceUtils.java
+++ src/main/java/com/dcsoft/aims/utils/DataSourceUtils.java
@@ -1,6 +1,7 @@
 package com.dcsoft.aims.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,9 +16,10 @@ public class DataSourceUtils {
 	
 	private static Map<String, DataSource> dataSourceMap = new HashMap<String, DataSource>();
 	
-	public static DataSource init(String username, String password, String url) throws Exception {
+	public static DataSource init(String driverClassName, String username, String password, String url, String connectionProperties) throws Exception {
 		// 创建连接池
 		Map<String, String> properties = new HashMap<String, String>();
+		properties.put("driverClassName", driverClassName);
 		properties.put("username", username);
 		properties.put("password", password);
 		properties.put("url", url);
@@ -25,6 +27,10 @@ public class DataSourceUtils {
 		properties.put("minIdle", "1");
 		properties.put("maxActive", "10");
 		properties.put("poolPreparedStatements", "true");
+		// 修改字符集
+		if (connectionProperties != null && !"".equals(connectionProperties)) {
+			properties.put("connectionProperties", connectionProperties);
+		}
 		return DruidDataSourceFactory.createDataSource(properties);
 	}
 	
@@ -36,6 +42,10 @@ public class DataSourceUtils {
 		String password = dbParams.get("target_db_password");
 		String dbType = dbParams.get("target_db_type");
 		String dbName = dbParams.get("target_db_name");
+		// 数据库字符集
+		String encoding = dbParams.get("target_db_encoding");
+		// 连接方式,针对oracle存在SID,SERVER_NAME两种连接情况进行相应处理
+		String connectMode = dbParams.get("target_db_connectMode");
 		// 连接池map中的key ip : 端口 : 数据库名 : 用户名 : 密码
 		StringBuffer keyBuffer = new StringBuffer();
 		keyBuffer.append(host).append(":").append(port).append(":").append(dbName).append(":")
@@ -44,17 +54,43 @@ public class DataSourceUtils {
 		if (key != null && !"".equals(key)) {
 			DataSource dataSource = dataSourceMap.get(key);
 			if (null == dataSource) {
+				String driverClassName = null;
+				// 需要进行转码的配置
+				StringBuffer connectionPropertiesBuffer = new StringBuffer();
 				// 连接数据库的url
 				StringBuffer urlBuffer = new StringBuffer();
 				switch (dbType) {
 					case "oracle" :
-						urlBuffer.append("jdbc:oracle:thin:@").append(host).append(":").append(port).append(":").append(dbName);
+						driverClassName = "oracle.jdbc.OracleDriver";
+						if (encoding != null && !"".equals(encoding)) {
+							// 针对US7ASII字符集特殊处理 url增加jdbc:wrap-jdbc:filters=encoding: 连接池增加配置 connectionProperties
+							urlBuffer.append("jdbc:wrap-jdbc:filters=encoding:");
+							connectionPropertiesBuffer.append("serverEncoding=").append(encoding).append(";clientEncoding=GBK;defaultRowPrefetch=50;bigStringTryClob=true");
+						}
+						/* 
+						 * url默认按照SID拼接
+						 * jdbc:oracle:thin:@localhost:1521:SID 
+						 * jdbc:oracle:thin:@//localhost:1521:SERVICENAME
+						 * jdbc:oracle:thin:@TNSName
+						 */
+						if (connectMode != null && !"".equals(connectMode)) {
+							if ("SERVICE_NAME".equals(connectMode)) {
+								urlBuffer.append("jdbc:oracle:thin:@//").append(host).append(":").append(port).append("/").append(dbName);
+							} else if ("TNS_NAME".equals(connectMode)) {
+								urlBuffer.append("jdbc:oracle:thin:@").append(dbName);
+							} else {
+								urlBuffer.append("jdbc:oracle:thin:@").append(host).append(":").append(port).append(":").append(dbName);
+							}
+						} else {
+							urlBuffer.append("jdbc:oracle:thin:@").append(host).append(":").append(port).append(":").append(dbName);
+						}
 						break;
 					default : 
 						break;
 				}
+				String connectionProperties = connectionPropertiesBuffer.toString();
 				String url = urlBuffer.toString();
-				dataSource = init(username, password, url);
+				dataSource = init(driverClassName, username, password, url, connectionProperties);
 				dataSourceMap.put(key, dataSource);
 			}
 			return dataSource;
@@ -85,6 +121,49 @@ public class DataSourceUtils {
     
     public static void close(Connection conn, Statement stmt) {
         close(conn, stmt, null);
+    }
+    
+    /**
+     * 通过驱动包获取connection连接
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public static Connection getConnection(Params params) throws Exception {
+		Map<String, String> dbParams = params.getDbParams();
+		String host = dbParams.get("target_db_host");
+		String port = dbParams.get("target_db_port");
+		String username = dbParams.get("target_db_user");
+		String password = dbParams.get("target_db_password");
+		String dbType = dbParams.get("target_db_type");
+		String dbName = dbParams.get("target_db_name");
+		String connectMode = dbParams.get("target_db_connectMode");
+		
+		String driverClassName = null;
+		StringBuffer urlBuffer = new StringBuffer();
+		switch (dbType) {
+			case "oracle" :
+				driverClassName = "oracle.jdbc.OracleDriver";
+				if (connectMode != null && !"".equals(connectMode)) {
+					if ("SERVICE_NAME".equals(connectMode)) {
+						urlBuffer.append("jdbc:oracle:thin:@//").append(host).append(":").append(port).append("/").append(dbName);
+					} else if ("TNS_NAME".equals(connectMode)) {
+						urlBuffer.append("jdbc:oracle:thin:@").append(dbName);
+					} else {
+						urlBuffer.append("jdbc:oracle:thin:@").append(host).append(":").append(port).append(":").append(dbName);
+					}
+				} else {
+					urlBuffer.append("jdbc:oracle:thin:@").append(host).append(":").append(port).append(":").append(dbName);
+				}
+				break;
+			default : 
+				break;
+		}
+		
+		String url = urlBuffer.toString();
+		Class.forName(driverClassName);
+		
+    	return DriverManager.getConnection(url, username, password);
     }
     
 }
